4a55ebea070bc5cf130179daab294ae72181bd18
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2022-08-20 11:34:55 -0700

    Format with Spotless

--- src/main/java/frc/robot/imu/ImuSubsystem.java
+++ src/main/java/frc/robot/imu/ImuSubsystem.java
@@ -5,7 +5,6 @@
 package frc.robot.imu;
 
 import com.ctre.phoenix.sensors.Pigeon2;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -23,6 +22,5 @@ public class ImuSubsystem extends SubsystemBase {
 
   public Rotation2d getRobotHeading() {
     return Rotation2d.fromDegrees(imu.getYaw());
-
   }
 }
