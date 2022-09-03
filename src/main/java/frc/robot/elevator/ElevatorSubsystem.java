d6ed46a3e8046b749d481c6970090219d3273e07
thebrossj2 <87281874+thebrossj2@users.noreply.github.com>
2022-09-02 18:28:22 -0700

    Start setting up MotionMagic

--- src/main/java/frc/robot/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/elevator/ElevatorSubsystem.java
@@ -4,7 +4,10 @@
 
 package frc.robot.elevator;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.misc.util.GearingConverter;
+import frc.robot.misc.util.sensors.SensorUnitConverter;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
@@ -12,6 +15,12 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class ElevatorSubsystem extends SubsystemBase {
   private TalonFX motor;
+  private static GearingConverter SPROCKET_TO_CHAIN = new GearingConverter(1/Units.inchesToMeters(1.7507));
+  private static GearingConverter GEARING = new GearingConverter(60);
+
+
+
+
 
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem(TalonFX motor) {
@@ -20,6 +29,9 @@ public class ElevatorSubsystem extends SubsystemBase {
     motor.config_kP(0, 0);
     motor.config_kI(0, 0);
     motor.config_kD(0, 0);
+    motor.configMotionAcceleration(0);
+    motor.configMotionCruiseVelocity(0);
+    motor.configClosedloopRamp(0);
   }
 
   @Override
@@ -35,6 +47,11 @@ public class ElevatorSubsystem extends SubsystemBase {
   public void setHeight(double height) {
     // Move the elevator to a certain height based on encoder readings
     // motor.set(ControlMode.)
+    final var heightBeforeSprocket = SPROCKET_TO_CHAIN.afterToBeforeGearing(height);
+    final var heightBeforeGearing = GEARING.afterToBeforeGearing(heightBeforeSprocket);
+    final var heightBeforeGearingSensorUnits = SensorUnitConverter.talonFX.radiansToSensorUnits(heightBeforeGearing);
+    motor.set(ControlMode.MotionMagic, heightBeforeGearingSensorUnits);
+
 
   }
 
