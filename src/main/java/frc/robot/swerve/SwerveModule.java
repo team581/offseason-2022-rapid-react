7b0a0011bbe820ed94c36b7cea0734b2996ff1d6
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2022-10-18 17:42:33 -0700

    Changed max velocity

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -19,7 +19,7 @@ import frc.robot.misc.util.GearingConverter;
 import frc.robot.misc.util.sensors.SensorUnitConverter;
 
 public class SwerveModule {
-  private static final double MAX_VELOCITY = 4.5;
+  private static final double MAX_VELOCITY = 13.5;
   private static final SimpleMotorFeedforward DRIVE_MOTOR_FEEDFORWARD =
       new SimpleMotorFeedforward(0, 0);
   private static final double DRIVE_MOTOR_MAX_VOLTAGE = 12;
