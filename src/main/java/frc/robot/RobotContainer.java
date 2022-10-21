74b2a829da2229d074c278a7afb7b2987efc8039
Jonah Snider <jonah@jonahsnider.com>
2022-10-21 06:35:41 -0700

    Format with Spotless

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -100,14 +100,14 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    //driver controls
+    // driver controls
     driverController.leftTrigger.whileActiveContinuous(
         new IntakeCommand(this.intakeSubsystem, IntakeMode.INTAKING)
             .alongWith(new WristCommand(this.wristSubsystem, WristPosition.INTAKING)));
     driverController.leftBumper.whileActiveContinuous(
         new WristCommand(this.wristSubsystem, WristPosition.OUTTAKING)
             .andThen(new IntakeCommand(this.intakeSubsystem, IntakeMode.OUTTAKING)));
-    //operator controls
+    // operator controls
     operatorController.leftTrigger.whileActiveContinuous(
         new WristCommand(this.wristSubsystem, WristPosition.INTAKING));
     operatorController.leftBumper.whileActiveContinuous(
