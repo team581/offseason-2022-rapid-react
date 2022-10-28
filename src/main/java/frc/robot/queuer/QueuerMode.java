15f2fe0db4ad88ee1eebb41b125b193ba3f761f4
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2022-10-27 18:27:51 -0700

    Add queuer

---
+++ src/main/java/frc/robot/queuer/QueuerMode.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.queuer;
+
+public enum QueuerMode {
+  STOPPED(0),
+  QUEUEING(0.25);
+
+  public final double percentage;
+
+  private QueuerMode(double percentage) {
+    this.percentage = percentage;
+  }
+}
