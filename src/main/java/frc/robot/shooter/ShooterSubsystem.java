894216c2abcf2d82dfba70c65733301f02018dff
rkanemoto <ryankanemoto@gmail.com>
2022-11-09 20:16:46 -0800

    Lower current limit from 30 to 25

--- src/main/java/frc/robot/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/shooter/ShooterSubsystem.java
@@ -40,7 +40,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
     this.motor.setIdleMode(CANSparkMax.IdleMode.kCoast);
     this.motor.enableVoltageCompensation(voltageCompensationReference);
-    this.motor.setSmartCurrentLimit(30);
+    this.motor.setSmartCurrentLimit(25);
 
     this.motor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 40);
     this.motor.setPeriodicFramePeriod(PeriodicFrame.kStatus1, 100);
